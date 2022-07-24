baf06a8c91e7d527fe09d5b7d138d2a31d0cf5c2
vchumack <54703870+vchumack@users.noreply.github.com>
2022-07-24 14:31:51 +0000

    Deploying to gh-pages from @ vchumack/team-hardcode@2ef3c69e9a8dea16ef139848ee651c4ea6cd4140 🚀

---
+++ index.a1f55b80.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll("[data-menu-button]"),t=document.querySelector("[data-menu]");e.forEach((function(e){e.addEventListener("click",(function(){e.classList.toggle("is-open"),t.classList.toggle("is-open")}))}))}();
+//# sourceMappingURL=index.a1f55b80.js.map
